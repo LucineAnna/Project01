f877fe9e8ae1a204b4f6b20ccf9dd63f0f75010e
LucineAnna <906243432@qq.com>
2022-04-28 17:48:39 +0800

    git-test

---
+++ JavaWeb/src/com/anna/java/GitTest.java
@@ -0,0 +1,11 @@
+package com.anna.java;
+
+/**
+ * @author:anna
+ * @date:2022/4/28-17:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
